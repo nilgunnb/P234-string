fc22e33b18ed2862e131db4959114b1a8265873b
nilgunnb <nilgunnb@code.edu.az>
2023-04-01 23:44:41 +0400

    added Program2.cs new file

---
+++ Program2.cs
@@ -0,0 +1,24 @@
+﻿// TASK 2:
+
+int[] Numbers = { 1, 2, 6, -5, -8, 9, -10, 13 };
+
+int Negativeount = 0;
+
+int PositiveCount = 0;
+
+for (int i = 0; i < Numbers.Length; i++)
+{
+    if (Numbers[i] < 0)
+    {
+        Negativeount++;
+    }
+    else if (Numbers[i] > 0)
+    {
+        PositiveCount++;
+    }
+}
+
+Console.WriteLine( Negativeount );
+
+
+Console.WriteLine(PositiveCount);
