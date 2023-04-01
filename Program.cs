83eb4e22295cedad377169a18c620d8919e4fff2
nilgunnb <nilgunnb@code.edu.az>
2023-04-01 22:52:51 +0400

    added Program.cs new file

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.Design;
+//TASK 1:
+
+int Points = 0;
+
+
+if (Points < 51 && Points <= 0)
+{
+    Console.WriteLine("Kesildiniz");
+}
+
+
+else if (Points >= 51 && Points <= 60)
+{
+    Console.WriteLine("E");
+}
+
+
+else if (Points >= 61 && Points <= 70)
+{
+    Console.WriteLine("D");
+}
+
+
+else if (Points >= 71 && Points <= 80)
+{
+    Console.WriteLine("C");
+}
+
+
+else if (Points >= 81 && Points <= 90)
+{
+    Console.WriteLine("B");
+}
+
+
+else if ( Points >= 91 && Points <= 100)
+{
+    Console.WriteLine("A");
+}
+
+
+else if (Points < 0 || Points > 100)
+{
+    Console.WriteLine( "Points are not valid" );
+}
