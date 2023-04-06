895ec322c0342e55b7945a40e924e24d110dbfd3
nilgunnb <nilgunnb@code.edu.az>
2023-04-06 10:58:19 +0400

    Added Programsubstring.cs new file

---
+++ Programsubstring.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+
+
+
+
+
+string Name = "LeonardoDavinci";
+
+
+CustomSubstring( Name, 3, 2);
+
+
+string CustomSubstring ( string key, int startindex, int Length)
+{ 
+    
+    try
+    {
+        string Name = key.Substring(startindex, Length);
+    }
+
+    catch ( Exception ex) 
+    {
+        Console.WriteLine("One of the indexes or both of them are not valid. please, check your work.");
+    }
+
+
+    return Name;
+    
+}
+
+
+Name = Name.Substring(3,2);
+
+Console.WriteLine(Name);
+
